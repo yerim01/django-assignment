a4611ba2f306a5c481c844008269f5e4044eee2e
Yerim Moon <myr9901@gmail.com>
2025-09-08 16:10:24 -0700

    feat: add viewset and urls for category and tags

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from shop.views import ProductViewSet
+from shop.views import ProductViewSet, CategoryViewSet, TagViewSet
 
 router = DefaultRouter()
-router.register(r"product", ProductViewSet, basename="product")
+router.register(r"products", ProductViewSet, basename="product")
+router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"tags", TagViewSet, basename="tag")
 
 urlpatterns = router.urls
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from shop.models import Product
-from shop.serializers import ProductSerializer
+from shop.models import Product, Category, Tag
+from shop.serializers import ProductSerializer, CategorySerializer, TagSerializer
 from rest_framework.permissions import AllowAny
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,4 +19,16 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ["description"] # Search by description
     filterset_fields = ["category", "tags"] # Filter by category or tags
+
+# Viewset for listing categories.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+# Viewset for listing tags
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [AllowAny]
     
