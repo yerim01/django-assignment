a5236cddd7d591d3f34e0b662990331b4186130e
Yerim Moon <myr9901@gmail.com>
2025-09-08 22:04:53 -0700

    fix: prevent allowing duplicate category and tag names

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Category(models.Model):
     """
     Category model: represents a product Category
     """    
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    def clean(self):
+        """
+        Check Validity before saving.
+        Ensures no duplicate Category exists with the same name.
+        Raises a ValidationError if a duplicate is found.
+        """
+        normalized_name = self.name.lower().strip()
+        if Category.objects.filter(name=normalized_name).exclude(pk=self.pk).exists():
+            raise ValidationError({"name": "Category with this name already exists."})
+
+    def save(self, *args, **kwargs):
+        """
+        Convert the name to lowercase and save the object.
+        """
+        self.name = self.name.lower().strip()
+        super().save(*args, **kwargs)
 
     def __str__(self): 
         return self.name
@@ -13,7 +31,16 @@ class Tag(models.Model):
     """
     Tag model: allows products to have mutiple tags
     """
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def clean(self):
+        normalized_name = self.name.lower().strip()
+        if Tag.objects.filter(name=normalized_name).exclude(pk=self.pk).exists():
+            raise ValidationError({"name": "Category with this name already exists."})
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower().strip()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
